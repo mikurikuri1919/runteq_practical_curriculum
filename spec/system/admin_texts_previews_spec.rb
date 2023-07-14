f408783c6bae3255e661509e2fdca34196621e7a
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-07-14 16:10:55 +0900

    Fix: error when viewing preview page without entering text in content block

---
+++ spec/system/admin_texts_previews_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe "admin_texts_previews", type: :system do
+  let(:user) { create(:user, :admin) }
+  describe '記事作成画面で画像ブロックを追加' do
+    context '画像ファイルをアップロードしない場合' do
+      it 'エラーが発生しない' do
+        login(user)
+        visit new_admin_article_path
+        fill_in 'article[title]', with: 'test'
+        fill_in 'article[slug]', with: 'test'
+        fill_in 'article[description]', with: 'test'
+        click_button '登録する'
+        click_link 'ブロックを追加する'
+        click_link '文章'
+        click_link 'プレビュー'
+        expect(page).not_to have_content("no implicit conversion of nil into String"), 'エラーページが表示されています'
+        expect(page).to have_content('test'), 'プレビューページが正しく表示されていません'
+      end
+    end
+  end
+end
+
