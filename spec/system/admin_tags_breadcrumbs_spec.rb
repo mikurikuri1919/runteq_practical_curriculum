953883b4f9aab66f5eb3c9ca8d8dd77406dc3ed0
mikurikuri1919 <mikuriyakenta003@gmail.com>
2023-07-14 12:00:31 +0900

    Add: breadcrumb link URL validation test

--- spec/system/admin_tags_breadcrumbs_spec.rb
+++ spec/system/admin_tags_breadcrumbs_spec.rb
@@ -2,28 +2,46 @@ require "rails_helper"
 
 RSpec.describe "admin_tags_breadcrumbs", type: :system do
   let(:user) { create(:user, :admin) }
-  # let(:tag) { create(:tag) }
 
   before do
     login(user)
   end
 
-  context 'タグ一覧画面にアクセス' do
-    example 'パンくずリストが表示される' do
-      visit admin_tags_path
+  describe 'タグ一覧画面' do
+    before { visit admin_tags_path }
+
+    it 'パンくずリストが表示される' do
       expect(page).to have_selector('.breadcrumb', text: 'Home')
       expect(page).to have_selector('.breadcrumb', text: 'タグ')
     end
+
+    it '「Home」リンクが機能している' do
+      click_link 'Home'
+      expect(page).to have_current_path(admin_dashboard_path)
+    end
   end
 
-  context 'タグ編集画面にアクセス' do
-    let!(:tag) { create :tag }
-    example 'パンくずリストが表示される' do
-      visit edit_admin_tag_path(tag)
+  describe 'タグ編集画面' do
+    let!(:tag) { create(:tag) }
+
+    before { visit edit_admin_tag_path(tag) }
+
+    it 'パンくずリストが表示される' do
       expect(page).to have_selector('.breadcrumb', text: 'Home')
       expect(page).to have_selector('.breadcrumb', text: 'タグ')
       expect(page).to have_selector('.breadcrumb', text: 'タグ編集')
     end
+
+    it '「Home」リンクが機能している' do
+      visit admin_tags_path
+      click_link 'Home'
+      expect(page).to have_current_path(admin_dashboard_path)
+    end
+
+    it '「タグ」リンクが機能している' do
+      visit admin_tags_path
+      click_link 'タグ'
+      expect(page).to have_current_path(admin_tags_path)
+    end
   end
 end
-
